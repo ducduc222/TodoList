b65a4eecd19c78c1bbc8079c476a753d3b71d123
ducduc222 <ducduc28052002@gmail.com>
2022-03-18 18:36:52 +0700

    change Code

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,8 @@ git init
 git add .
 git commit -m "New Deployment"
 git push -f git@github.com:ducduc222/todolist.git master:review-page
+
+cd ..
+git add .
+git commit -m "change Code"
+git push origin main
